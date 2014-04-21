9b7205add708a9d64d5333ea432935b0f00fb612
Evan Lindsay <evanlindsay@live.com>
2014-04-21 03:11:58 -0700

    Use a wrapper class for MongoClient

--- src/main/java/com/mcprohosting/plugins/av/datastoragemanager/DataStorageManager.java
+++ src/main/java/com/mcprohosting/plugins/av/datastoragemanager/DataStorageManager.java
@@ -1,12 +1,24 @@
 package com.mcprohosting.plugins.av.datastoragemanager;
 
+import com.mcprohosting.plugins.av.datastoragemanager.database.MongoResource;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DataStorageManager extends JavaPlugin {
 
+    private static DataStorageManager instance;
+    private static MongoResource mongoResource;
 
     public void onEnable() {
+        instance = this;
+        mongoResource = new MongoResource();
+    }
 
+    public static DataStorageManager getInstance() {
+        return instance;
+    }
+
+    public static MongoResource getMongoResource() {
+        return mongoResource;
     }
 
 }
---
+++ src/main/java/com/mcprohosting/plugins/av/datastoragemanager/database/MongoResource.java
@@ -0,0 +1,86 @@
+package com.mcprohosting.plugins.av.datastoragemanager.database;
+
+import com.mcprohosting.plugins.av.datastoragemanager.DataStorageManager;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.Morphia;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.Set;
+
+public class MongoResource {
+    private MongoClient mongoClient;
+    private Properties properties;
+
+    public MongoResource() {
+        DataStorageManager.getInstance().saveResource("mongodb.properties", false);
+
+        try {
+            if (properties == null) {
+                try {
+                    properties = loadProperties();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (mongoClient == null) {
+                mongoClient = getClient();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Properties loadProperties() throws IOException {
+        Properties properties = new Properties();
+        InputStream inputStream = DataStorageManager.getInstance().getResource("mongodb.properties");
+
+        if (inputStream == null) {
+            throw new FileNotFoundException("Unable to load mongodb.properties!");
+        }
+
+        properties.load(inputStream);
+        return properties;
+    }
+
+    private MongoClient getClient() {
+        try {
+            if (!properties.getProperty("username").isEmpty() &&
+                    !properties.getProperty("password").isEmpty()) {
+                MongoCredential credential = MongoCredential.createMongoCRCredential(
+                        properties.getProperty("username"), "antvenomnetwork",
+                        properties.getProperty("password").toCharArray()
+                );
+                return new MongoClient(new ServerAddress(
+                        properties.getProperty("host"),
+                        Integer.valueOf(properties.getProperty("port"))), Arrays.asList(credential));
+            } else {
+                return new MongoClient(
+                        properties.getProperty("host"),
+                        Integer.valueOf(properties.getProperty("port")));
+            }
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public Datastore getDatastore(String dbName) {
+        Datastore datastore;
+
+        datastore = new Morphia((Set) new HashSet<Class<?>>(DataStorageManager.getInstance().getDatabaseClasses())).
+                createDatastore(mongoClient, dbName);
+
+        return datastore;
+    }
+}
